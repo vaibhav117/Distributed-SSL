4b5310c1c0683f01105c39cf3103e88d78982e2a
vaibhav117 <vaibhavmathur117@gmail.com>
2022-05-15 03:47:25 -0400

    Fixing image dim

--- augmentations.py
+++ augmentations.py
@@ -33,22 +33,22 @@ class TrainTransform(object):
             self.mean = [0.485, 0.456, 0.406]
             self.std = [0.229, 0.224, 0.225]
             self.crop_size = 224
-            self.padding = 0
+            self.padding = 30
         elif dataset == 'tiny-imagenet':
             self.mean = [0.485, 0.456, 0.406]
             self.std = [0.229, 0.224, 0.225]
             self.crop_size = 64
-            self.padding = 4
+            self.padding = 6
         elif dataset == 'CIFAR10':
             self.mean = [0.4914, 0.4822, 0.4465]
             self.std = (0.2023, 0.1994, 0.2010)
             self.crop_size = 32
-            self.padding = 4
+            self.padding = 6
             
         self.transform = transforms.Compose(
             [
-                transforms.RandomResizedCrop(
-                    self.crop_size, interpolation=InterpolationMode.BICUBIC, padding=self.padding
+                transforms.RandomCrop(
+                    self.crop_size, padding=self.padding
                 ),
                 transforms.RandomHorizontalFlip(p=0.5),
                 transforms.RandomApply(
@@ -70,8 +70,8 @@ class TrainTransform(object):
         )
         self.transform_prime = transforms.Compose(
             [
-                transforms.RandomResizedCrop(
-                    self.crop_size, interpolation=InterpolationMode.BICUBIC, padding=self.padding
+                transforms.RandomCrop(
+                    self.crop_size, padding=self.padding
                 ),
                 transforms.RandomHorizontalFlip(p=0.5),
                 transforms.RandomApply(
